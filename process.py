09c107225ff26d911820a50eccb13bde04d526b0
manassess <manasses3mm@gmail.com>
2019-07-22 14:17:00 -0300

    some changes and I added an function to show a slice

--- process.py
+++ process.py
@@ -5,10 +5,11 @@ import os
 import numpy as np
 from hunsfield import get_pixels_hu
 import copy
+import random
 
 def load_FilesDicom (path) :
     """
-    read all the dicom imagens in a folder
+    load all the dicom imagens in a folder
     
     :parameters path: an path that leads to the source directory of images
     :returns filesDicom: an dataset with all the dicom imagens in the source directory
@@ -38,16 +39,27 @@ def load_PathDirs(path):
     
     return paths
 
-#getting the paths of all the repositorys with dicom images
+def show(im):
+    """
+    display an slice
+
+    :parameter im: the slice to display
+    :return:
+    """
+    plt.imshow(im, cmap='bone')
+    plt.show()
+
+
+#getting the paths of all the repositorys (volumes) with dicom images
 path = "C:/Users/Manassés/Documents/Ciencia_da_Computacao/Pesquisa/LCTSC/Train"
 dirs = load_PathDirs(path)
 
-#getting the dicom images in the repositorys with them
-print(dirs[0])
-for i in dirs:
-    dataset = load_FilesDicom(i)
+#getting an volume randomly and loading all of the images (slices) inside it
+volume = copy.copy(random.choice(dirs))
+print(volume)
+dataset = load_FilesDicom(volume)
 
-#selecting an image from the dataset tos use as example 
-current = copy.copy(dataset[300])
-plt.imshow(current.pixel_array, cmap='bone')
-plt.show()
+#selecting an slice randomly to use to get the standart template 
+slice = copy.copy(random.choice(dataset))
+slice  = get_pixels_hu(slice)
+
